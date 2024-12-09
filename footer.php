f03675de3d6131dbbd6b5993c7e4f0bf038b904e
Goncharova-Dasha <139782357+Goncharova-Dasha@users.noreply.github.com>
2024-12-09 12:51:55 +0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
         <p>&copy; 2024 Приют для котиков. Все права защищены.</p>
         <p>Следите за нами в социальных сетях:</p>
         <ul>
-            <li><a href="#">Instagram</a></li>
+            <li><a href="#">Rutube</a></li>
             <li><a href="#">VK</a></li>
         </ul>
     </footer>
